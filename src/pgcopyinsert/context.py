257c396575ff3843a09bd7a7f4683a4f18c4e1a0
Odos Matthews <odosmatthews@odoss-mbp.lan>
2024-04-05 09:58:12 -0400

    add async copyinsert fuctions

--- src/pgcopyinsert/context.py
+++ src/pgcopyinsert/context.py
@@ -1,9 +1,11 @@
 import typing as _t
 
-from contextlib import contextmanager
+from contextlib import contextmanager, asynccontextmanager
 
 import sqlalchemy as sa
 from sqlalchemy.engine.interfaces import DBAPICursor
+from sqlalchemy.ext.asyncio import AsyncConnection
+import asyncpg
 
 
 @contextmanager
@@ -22,3 +24,22 @@ def raw_cursor(raw_connection: sa.PoolProxiedConnection) -> _t.Generator[DBAPICu
         yield cursor
     finally:
         cursor.close()
+
+
+@asynccontextmanager
+async def async_raw_connection(async_connection: AsyncConnection) -> _t.AsyncGenerator[sa.PoolProxiedConnection, None]:
+    connection: sa.PoolProxiedConnection = await async_connection.get_raw_connection()
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
+@asynccontextmanager
+async def asyncpg_connection(async_connection: AsyncConnection) -> _t.AsyncGenerator[asyncpg.Connection, _t.Any]:
+    async with async_raw_connection(async_connection) as connection:
+        asyncpg_con: asyncpg.Connection = connection.driver_connection # type: ignore
+        try:
+            yield asyncpg_con
+        finally:
+            await asyncpg_con.close()
